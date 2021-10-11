0c60e2b587be4a1041a444e90585f4d0e7b2790b
Ian Whitmore <ianwhitmor@gmail.com>
2021-10-10 23:05:36 -0500

    game is now running with both players and X/O are showing on new game board

--- Lab04_TicTacToe/Lab04_TicTacToe/Classes/Game.cs
+++ Lab04_TicTacToe/Lab04_TicTacToe/Classes/Game.cs
@@ -56,6 +56,7 @@ namespace Lab04_TicTacToe.Classes
 				Player newPlayer = NextPlayer();
 				newPlayer.TakeTurn(Board);
 				SwitchPlayer();
+				Board.DisplayBoard();
 				}
 
 			return PlayerOne;
